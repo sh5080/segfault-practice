3a4faa86dd7da7f4c67062d6523788bc8d8b1e88
sh5080 <prpn97@gmail.com>
2024-04-27 22:49:40 +0900

    fix: signup user api로 이동

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,51 +1,34 @@
 import { AxiosResponse } from "axios";
 import { LoginResponse } from "type/response.type";
-
 import ApiManager from "./instance";
 
-ApiManager.initializeInstances();
-const defaultUrl = "auth";
+export default class AuthApi {
+  private static defaultUrl = "auth";
 
-export const loginApi = async (
-  email: string,
-  password: string
-): Promise<LoginResponse> => {
-  try {
-    const response: AxiosResponse<LoginResponse> =
-      await ApiManager.default().post(`/${defaultUrl}/login`, {
-        email,
-        password,
-      });
-    return response.data;
-  } catch (err) {
-    console.error(err);
-    throw err;
+  static async login(email: string, password: string): Promise<LoginResponse> {
+    try {
+      console.log("email: ", email, password);
+      const response: AxiosResponse<LoginResponse> =
+        await ApiManager.default().post(`/${this.defaultUrl}/login`, {
+          email,
+          password,
+        });
+      return response.data;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
-};
 
-export const logoutApi = async () => {
-  try {
-    const response = await ApiManager.default().delete(`/${defaultUrl}/logout`);
-    return response.data;
-  } catch (err) {
-    console.error(err);
-    throw err;
+  static async logout() {
+    try {
+      const response = await ApiManager.default().delete(
+        `/${this.defaultUrl}/logout`
+      );
+      return response.data;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
-};
-export const signupApi = async (
-  name: string,
-  email: string,
-  password: string
-) => {
-  try {
-    const response = await ApiManager.default().post(`/${defaultUrl}/signup`, {
-      name,
-      email,
-      password,
-    });
-    return response.data;
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
-};
+}
